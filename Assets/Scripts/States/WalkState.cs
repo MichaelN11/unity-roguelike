dd6cd54a83d06aff8d44b646dcd1b63d923de946
MichaelN <micneet@gmail.com>
2023-01-07 18:07:36 -0500

    deleted ActionState classes

--- Assets/Scripts/States/WalkState.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-public class WalkState : ActionState
-{
-    public WalkState(EntityState entityState) : base(entityState) { }
-
-    public override bool CanAttack => true;
-    public override bool CanMove => true;
-    public override bool CanLook => true;
-}
