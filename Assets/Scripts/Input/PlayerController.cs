22b113ade766f6ba1b9fe17145bf41b66873ce5a
MichaelN <micneet@gmail.com>
2023-01-22 17:34:08 -0500

    hold mouse button to attack. switched input system to use c# events

--- Assets/Scripts/Input/PlayerController.cs
+++ Assets/Scripts/Input/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
 
 /// <summary>
 /// Singleton component for handling player input for the entity, using the Input System.
@@ -13,10 +14,12 @@ public class PlayerController : MonoBehaviour
 
     public static PlayerController Instance { get; private set; }
 
+    private PlayerInputActions inputActions;
     private EntityController entityController;
     private Vector2 lookDirection = Vector2.down;
     private InputData bufferedInput = null;
     private float bufferTimer = 0f;
+    private bool isFireHeld = false;
 
     private void Awake()
     {
@@ -30,10 +33,22 @@ public class PlayerController : MonoBehaviour
         }
 
         entityController = GetComponent<EntityController>();
+
+        inputActions = new PlayerInputActions();
+        inputActions.Enable();
+        inputActions.Player.Fire.started += OnFireStarted;
+        inputActions.Player.Fire.canceled += OnFireCancelled;
+        inputActions.Player.Move.performed += OnMovePerformed;
+        inputActions.Player.Move.canceled += OnMoveCancelled;
+        inputActions.Player.Look.performed += OnLook;
     }
 
     private void Update()
     {
+        if (isFireHeld)
+        {
+            FireHeld();
+        }
         if (bufferedInput != null && bufferTimer > 0)
         {
             bool updateSuccessful = entityController.UpdateFromInput(bufferedInput);
@@ -56,24 +71,30 @@ public class PlayerController : MonoBehaviour
     /// <summary>
     /// Called by the Input System. Sets the movement direction using the Vector2 from the InputValue.
     /// </summary>
-    /// <param name="inputValue">The InputValue containing a Vector2 direction to move in</param>
-    public void OnMove(InputValue inputValue)
+    /// <param name="ctx">The CallbackContext containing a Vector2 direction to move in</param>
+    public void OnMovePerformed(CallbackContext ctx)
     {
-        Vector2 moveDirection = inputValue.Get<Vector2>();
-        InputData inputData = new();
-        inputData.Type = InputType.Move;
-        inputData.Direction = moveDirection;
-        entityController.UpdateFromInput(inputData);
+        Vector2 moveDirection = ctx.ReadValue<Vector2>();
+        SendMoveInput(moveDirection);
+    }
+
+    /// <summary>
+    /// Called by the Input System. Sets the movement direction to zero.
+    /// </summary>
+    /// <param name="ctx">The CallbackContext</param>
+    public void OnMoveCancelled(CallbackContext ctx)
+    {
+        SendMoveInput(Vector2.zero);
     }
 
     /// <summary>
     /// Called by the Input System. Sets the look direction using the Vector2 from the InputValue,
     /// determining the input position relative to the transform position.
     /// </summary>
-    /// <param name="inputValue">The InputValue containing a Vector2 look position</param>
-    public void OnLook(InputValue inputValue)
+    /// <param name="ctx">The CallbackContext containing a Vector2 look position</param>
+    public void OnLook(CallbackContext ctx)
     {
-        Vector2 inputScreenPosition = inputValue.Get<Vector2>();
+        Vector2 inputScreenPosition = ctx.ReadValue<Vector2>();
         Vector2 inputWorldPosition = Camera.main.ScreenToWorldPoint(inputScreenPosition);
         Vector2 entityPosition = transform.position;
         Vector2 inputPositionRelativeToEntity = inputWorldPosition - entityPosition;
@@ -87,12 +108,12 @@ public class PlayerController : MonoBehaviour
     }
 
     /// <summary>
-    /// Called by the Input System. Triggers an attack action for the EntityController.
-    /// Buffers the input so it can be used when entity can next act.
+    /// Called by the Input System. Tried to attack, and sets the attack buffer if the attack failed.
     /// </summary>
-    /// <param name="inputValue">The InputValue</param>
-    public void OnFire(InputValue inputValue)
+    /// <param name="ctx">The callback context</param>
+    public void OnFireStarted(CallbackContext ctx)
     {
+        isFireHeld = true;
         InputData inputData = new();
         inputData.Type = InputType.Attack;
         inputData.Direction = lookDirection;
@@ -102,5 +123,38 @@ public class PlayerController : MonoBehaviour
             bufferedInput = inputData;
             bufferTimer = inputBuffer;
         }
+    }
+
+    /// <summary>
+    /// Called by the Input System. Sets fire held to false.
+    /// </summary>
+    /// <param name="ctx">The callback context</param>
+    public void OnFireCancelled(CallbackContext ctx)
+    {
+        isFireHeld = false;
+    }
+
+    /// <summary>
+    /// Actions taken while the fire button is held. Triggers an attack action for the EntityController.
+    /// Buffers the input so it can be used when entity can next act.
+    /// </summary>
+    private void FireHeld()
+    {
+        InputData inputData = new();
+        inputData.Type = InputType.Attack;
+        inputData.Direction = lookDirection;
+        entityController.UpdateFromInput(inputData);
+    }
+
+    /// <summary>
+    /// Sends the move input to the entity controller.
+    /// </summary>
+    /// <param name="moveDirection">The Vector2 move direction</param>
+    private void SendMoveInput(Vector2 moveDirection)
+    {
+        InputData inputData = new();
+        inputData.Type = InputType.Move;
+        inputData.Direction = moveDirection;
+        entityController.UpdateFromInput(inputData);
     }
 }
