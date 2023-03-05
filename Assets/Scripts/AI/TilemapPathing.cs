9577bb7db0c0ba0b8439679de92eeef3dbc36a97
MichaelN <micneet@gmail.com>
2023-03-05 00:52:09 -0500

    AI takes multiple tilemaps into consideration for pathing

--- Assets/Scripts/AI/TilemapPathing.cs
+++ Assets/Scripts/AI/TilemapPathing.cs
@@ -13,7 +13,7 @@ public class TilemapPathing : MonoBehaviour
 
     public PathingGrid PathingGrid { get; set; } = new();
 
-    private Tilemap tilemap;
+    private List<Tilemap> tilemapList;
 
     private void Awake()
     {
@@ -26,7 +26,7 @@ public class TilemapPathing : MonoBehaviour
             Instance = this;
         }
 
-        tilemap = GetComponent<Tilemap>();
+        tilemapList = GetComponentsInChildren<Tilemap>().ToList();
     }
 
     private void OnDestroy()
@@ -36,34 +36,62 @@ public class TilemapPathing : MonoBehaviour
 
     private void Start()
     {
-        BuildPathingGrid(tilemap);
+        tilemapList.RemoveAll(IsPassableLayer);
+
+        BuildPathingGrid(tilemapList);
         BuildAdjacentActions();
     }
 
     /// <summary>
-    /// Builds the PathingGrid from the passed Tilemap.
+    /// Search predicate for finding tilemaps that are passable, which shouldn't be used for pathing.
     /// </summary>
-    /// <param name="tilemap">The Tilemap used to build the grid</param>
-    private void BuildPathingGrid(Tilemap tilemap)
+    /// <param name="tilemap">The tilemap to check the layer</param>
+    /// <returns>true if the tilemap is using a passable layer</returns>
+    private bool IsPassableLayer(Tilemap tilemap)
     {
-        PathingGrid.CellWidth = tilemap.cellSize.x;
-        PathingGrid.Position = tilemap.localBounds.min;
+        return tilemap.gameObject.layer != LayerMask.NameToLayer("Wall");
+    }
 
-        BoundsInt bounds = tilemap.cellBounds;
-        TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+    /// <summary>
+    /// Builds the PathingGrid from the passed Tilemap list. Assumes the tilemap cells are the same size.
+    /// </summary>
+    /// <param name="tilemapList">The Tilemap list used to build the grid</param>
+    private void BuildPathingGrid(List<Tilemap> tilemapList)
+    {
+        Vector2 minPosition = Vector2.positiveInfinity;
+        Vector2 maxPosition = Vector2.negativeInfinity;
+        List<TileBase[]> allTilemapTilesList = new();
 
-        for (int x = 0; x < bounds.size.x; x++)
+        foreach (Tilemap tilemap in tilemapList)
+        {
+            Bounds localBounds = tilemap.localBounds;
+            minPosition.x = (localBounds.min.x < minPosition.x) ? localBounds.min.x : minPosition.x;
+            minPosition.y = (localBounds.min.y < minPosition.y) ? localBounds.min.y : minPosition.y;
+            maxPosition.x = (localBounds.max.x > maxPosition.x) ? localBounds.max.x : maxPosition.x;
+            maxPosition.y = (localBounds.max.y > maxPosition.y) ? localBounds.max.y : maxPosition.y;
+
+            BoundsInt bounds = tilemap.cellBounds;
+            TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+            allTilemapTilesList.Add(allTiles);
+        }
+
+        PathingGrid.CellWidth = tilemapList[0].cellSize.x;
+        PathingGrid.Position = minPosition;
+
+        int gridWidth = (int)((maxPosition.x - minPosition.x) / PathingGrid.CellWidth);
+        int gridHeight = (int)((maxPosition.y - minPosition.y) / PathingGrid.CellWidth);
+
+        for (int x = 0; x < gridWidth; x++)
         {
             List<GridNode> column = new();
             PathingGrid.Grid.Add(column);
-            for (int y = 0; y < bounds.size.y; y++)
+            for (int y = 0; y < gridHeight; y++)
             {
                 GridNode node = new();
                 node.X = x;
                 node.Y = y;
 
-                TileBase tile = allTiles[CalculateTileArrayPosition(x, y, bounds)];
-                node.Passable = IsNodePassable(tile);
+                node.Passable = IsPositionPassable(x, y, tilemapList, allTilemapTilesList, PathingGrid);
 
                 column.Add(node);
             }
@@ -71,12 +99,53 @@ public class TilemapPathing : MonoBehaviour
     }
 
     /// <summary>
-    /// Determines if the node corresponding the to the passed TileBase position
-    /// is a passable node. If there is a tile at the position, the node is not passable.
+    /// Determines if the passed position is passable using the passed list of tilemaps and list of all the tilemap tiles.
+    /// </summary>
+    /// <param name="x">The int x position of the cell in the grid</param>
+    /// <param name="y">The int y position of the cell in the grid</param>
+    /// <param name="tilemapList">The list of tilemaps</param>
+    /// <param name="allTilemapTilesList">The list of TileBase[] representing all of the tiles in each tilemap</param>
+    /// <param name="pathingGrid">The pathing grid</param>
+    /// <returns>true if the grid position is passable</returns>
+    private bool IsPositionPassable(int x, int y, List<Tilemap> tilemapList,
+        List<TileBase[]> allTilemapTilesList, PathingGrid pathingGrid)
+    {
+        bool passable = true;
+        for (int i = 0; i < tilemapList.Count; i++)
+        {
+            TileBase[] allTiles = allTilemapTilesList[i];
+            Tilemap tilemap = tilemapList[i];
+            BoundsInt cellBounds = tilemap.cellBounds;
+
+            Vector2 tilemapPosition = tilemap.localBounds.min;
+            Vector2 tilemapOffset = tilemapPosition - pathingGrid.Position;
+            Vector2Int tilemapCellOffset = Vector2Int.FloorToInt(tilemapOffset / pathingGrid.CellWidth);
+            int tilemapX = x - tilemapCellOffset.x;
+            int tilemapY = y - tilemapCellOffset.y;
+            
+            if (tilemapX >= 0
+                && tilemapY >= 0
+                && tilemapX < cellBounds.size.x
+                && tilemapY < cellBounds.size.y)
+            {
+                int arrayIndex = CalculateTileArrayPosition(tilemapX, tilemapY, cellBounds);
+                TileBase tile = allTiles[arrayIndex];
+                passable = passable && IsTilePassable(tile);
+                if (!passable)
+                {
+                    break;
+                }
+            }
+        }
+        return passable;
+    }
+
+    /// <summary>
+    /// Determines if the TileBase is passable. If there is a tile at the position, the node is not passable.
     /// </summary>
     /// <param name="tile">The tile corresponding to the position of the node</param>
     /// <returns>true if tile is null</returns>
-    private bool IsNodePassable(TileBase tile)
+    private bool IsTilePassable(TileBase tile)
     {
         return (tile == null);
     }
