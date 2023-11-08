32fcc52cbfe593ae4682b777ca91b24e9c3254f8
MichaelN <micneet@gmail.com>
2023-11-08 17:57:22 -0500

    flash material doesn't use resources folder

--- Assets/Scripts/Util/ResourceManager.cs
+++ Assets/Scripts/Util/ResourceManager.cs
@@ -9,10 +9,9 @@ public class ResourceManager : MonoBehaviour
 {
     public static ResourceManager Instance { get; private set; }
 
-    public Material FlashMaterial { get; private set; }
-
     [SerializeField]
-    private string flashMaterial = "Flash";
+    private Material flashMaterial;
+    public Material FlashMaterial => flashMaterial;
 
     [SerializeField]
     private GameObject itemPickupObject;
@@ -31,14 +30,5 @@ public class ResourceManager : MonoBehaviour
 
         transform.parent = null;
         DontDestroyOnLoad(gameObject);
-        LoadResources();
-    }
-
-    /// <summary>
-    /// Loads the static resources from the Resources folder.
-    /// </summary>
-    private void LoadResources()
-    {
-        FlashMaterial = (Material) Resources.Load(flashMaterial);
     }
 }
