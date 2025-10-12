e27c30713d566ae70a69fd05b8ea624c7e55aaaf
MichaelN <micneet@gmail.com>
2025-10-12 16:46:21 -0400

    wolf dash is no longer invincible

---
+++ Assets/Scripts/Ability/Effects/PassThroughEntitiesEffect.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// An AbilityEffect that causes the entity to pass through entities.
+/// </summary>
+[CreateAssetMenu(menuName = "Game Data/Ability Effect/Pass Through Entities")]
+public class PassThroughEntitiesEffect : AbilityEffect
+{
+    public override void Trigger(AbilityUseData abilityUseData, EffectUseData effectUseData)
+    {
+        abilityUseData.Movement.PassThroughEntities(Duration);
+    }
+
+    public override void Unapply(AbilityUseData abilityUseData, EffectUseData effectUseData)
+    {
+        abilityUseData.Movement.StopPassingThroughEntities();
+    }
+}
